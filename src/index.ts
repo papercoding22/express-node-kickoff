5679041778589f32bf4c50a3593b25384304592d
papercoding22 <john.ly997@gmail.com>
2024-04-06 11:34:11 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+require('module-alias/register');
+
+// Create simple express server
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
