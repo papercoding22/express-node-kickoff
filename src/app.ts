e7686c98956683d4ea77f6e82c649fcfd330d62d
papercoding22 <john.ly997@gmail.com>
2024-04-06 11:57:47 +0700

    add: setup middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,23 @@
-export const sum = (a: number, b: number): number => {
-  return a + b;
-};
+// Create simple express server
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import cors from 'cors';
 
-const result = sum(1, 2);
+const app = express();
 
-console.log(result);
+// Use body-parser middleware to parse incoming request bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Use morgan middleware for logging HTTP requests
+app.use(morgan('dev'));
+
+// Use cors middleware to handle Cross-Origin Resource Sharing
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+export default app;
